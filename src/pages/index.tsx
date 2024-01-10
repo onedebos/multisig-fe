cb6124da9630cbf20774d209b023aa8b339173b1
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2024-01-10 14:34:21 +0000

    add hooks and import hooks in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Sidebar from "@/components/Sidebar";
 import SubmitProposal from "@/components/SubmitProposal";
 import SeeProposals from "@/components/SeeProposals";
+import useTaquito from "@/hooks/useTaquito";
 import { useState } from "react";
 
 export default function Home() {
   const [isSeeProposalsScreen, setIsSeeProposalsScreen] = useState(true);
+  const { walletAddress, connectWallet, contractAddress } = useTaquito();
 
   const proposals = [
     {
@@ -14,8 +16,6 @@ export default function Home() {
       voters: ["tz1ayagi1KJN4Hfiu51CzBFTXuHabVhTUvzD", "tz1ayagi1KJN4Hfiu51CzBFTXuHabVhTUvzD"],
     },
   ];
-
-  const contractAddress = "KT1QfJbWR1Hg3R8FtSnmuPv4mpvekqZuZi7a";
 
   return (
     <main className="bg-gray-900 p-10 min-h-screen flex gap-4">
